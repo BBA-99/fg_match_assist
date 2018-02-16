e5f407a0c2d749c0592e2a3d4319d74413a43cdf
bba-99 <kenmochi@cardomain.co.jp>
2018-02-16 17:44:05 +0900

    update createRoom

--- constants/targetLevel.js
+++ constants/targetLevel.js
@@ -1,38 +1,38 @@
-export const TargetLevel = {
-    "ja":{
-        TARGETLEVEL_TYPE_DEPARTURE_BEGINNER:{
-            name:"脱初心者",
-            value:1
-        },
-        TARGETLEVEL_TYPE_ADVANCE:{
-            name:"応用技術を扱える",
-            value:2
-        },
-        TARGETLEVEL_TYPE_HIGHER_RANK:{
-            name:"より高いランクを目指す",
-            value:3
-        },
-        TARGETLEVEL_TYPE_ATHLETE:{
-            name:"大会で上位を目指す",
-            value:4
-        },
+export const TargetLevelType = {
+  'jp': {
+    TARGETLEVEL_TYPE_DEPARTURE_BEGINNER: {
+      name: '脱初心者',
+      value: 1
     },
-    "en":{
-        TARGETLEVEL_TYPE_DEPARTURE_BEGINNER:{
-            name:"departure beginner",
-            value:1
-        },
-        TARGETLEVEL_TYPE_ADVANCE:{
-            name:"utilizing advanced tactics",
-            value:2
-        },
-        TARGETLEVEL_TYPE_HIGHER_RANK:{
-            name:"higher rank",
-            value:3
-        },
-        TARGETLEVEL_TYPE_ATHLETE:{
-            name:"top athlete",
-            value:4
-        },
+    TARGETLEVEL_TYPE_ADVANCE: {
+      name: '応用技術を扱える',
+      value: 2
     },
+    TARGETLEVEL_TYPE_HIGHER_RANK: {
+      name: 'より高いランクを目指す',
+      value: 3
+    },
+    TARGETLEVEL_TYPE_ATHLETE: {
+      name: '大会で上位を目指す',
+      value: 4
+    }
+  },
+  'en': {
+    TARGETLEVEL_TYPE_DEPARTURE_BEGINNER: {
+      name: 'departure beginner',
+      value: 1
+    },
+    TARGETLEVEL_TYPE_ADVANCE: {
+      name: 'utilizing advanced tactics',
+      value: 2
+    },
+    TARGETLEVEL_TYPE_HIGHER_RANK: {
+      name: 'higher rank',
+      value: 3
+    },
+    TARGETLEVEL_TYPE_ATHLETE: {
+      name: 'top athlete',
+      value: 4
+    }
+  }
 }
--- pages/_lang/createRoom.vue
+++ pages/_lang/createRoom.vue
@@ -23,7 +23,7 @@
       </v-flex>
     </v-layout>
     <v-layout v-if="title != null">
-      <v-flex>
+      <v-flex xs6>
         <v-select
           :label="title_expLavel"
           v-model="title_exp"
@@ -31,6 +31,17 @@
           item-value="value"
           :items="title_exp_items"
           :rules="title_expRules"
+          required
+        ></v-select>
+      </v-flex>
+      <v-flex xs6>
+        <v-select
+          :label="targetLevelLavel"
+          v-model="targetLevel"
+          item-text='display'
+          item-value="value"
+          :items="targetLevel_items"
+          :rules="targetLevelRules"
           required
         ></v-select>
       </v-flex>
@@ -59,16 +70,19 @@
 import {DB} from '~/plugins/firebase'
 import {RoomConditionType} from '~/constants/roomCondition'
 import {TitleType} from '~/constants/title'
-import {ExpType as TitleExp} from '~/constants/exp'
+import {ExpType as TitleExpType} from '~/constants/exp'
+import {TargetLevelType} from '~/constants/targetLevel'
 
 export default {
   data () {
     const roomConditionItem = (this.$store.state.locale === 'jp') ? RoomConditionType.jp : RoomConditionType.en
     const titleItem = (this.$store.state.locale === 'jp') ? TitleType.jp : TitleType.en
-    const titleExpItem = (this.$store.state.locale === 'jp') ? TitleExp.jp : TitleExp.en
+    const titleExpItem = (this.$store.state.locale === 'jp') ? TitleExpType.jp : TitleExpType.en
+    const targetLevelItem = (this.$store.state.locale === 'jp') ? TargetLevelType.jp : TargetLevelType.en
     return {
       valid: true,
       roomCondition: roomConditionItem[this.$store.state.createRoom.roomCondition],
+      // タイトル
       title: this.$store.state.createRoom.title,
       titleLavel: this.$t('createRoom.title.lavel'),
       title_items: [
@@ -76,6 +90,7 @@ export default {
         { display: titleItem.DBFZ_X.name, value: titleItem.DBFZ_X.name }
       ],
       titleRules: [ (v) => !!v || this.$t('createRoom.title.required') ],
+      // やり込み度合い
       title_exp: this.$store.state.createRoom.title_exp,
       title_expLavel: this.$t('createRoom.title_exp.lavel'),
       title_exp_items: [
@@ -86,6 +101,16 @@ export default {
         { display: titleExpItem.EXP_TYPE_RANKER.name, value: titleExpItem.EXP_TYPE_RANKER.name }
       ],
       title_expRules: [ (v) => !!v || this.$t('createRoom.title_exp.required') ],
+      // 目標レベル
+      targetLevel: this.$store.state.createRoom.targetLevel,
+      targetLevelLavel: this.$t('createRoom.targetLevel.lavel'),
+      targetLevel_items: [
+        { display: targetLevelItem.TARGETLEVEL_TYPE_DEPARTURE_BEGINNER.name, value: targetLevelItem.TARGETLEVEL_TYPE_DEPARTURE_BEGINNER.name },
+        { display: targetLevelItem.TARGETLEVEL_TYPE_ADVANCE.name, value: targetLevelItem.TARGETLEVEL_TYPE_ADVANCE.name },
+        { display: targetLevelItem.TARGETLEVEL_TYPE_HIGHER_RANK.name, value: targetLevelItem.TARGETLEVEL_TYPE_HIGHER_RANK.name },
+        { display: targetLevelItem.TARGETLEVEL_TYPE_ATHLETE.name, value: targetLevelItem.TARGETLEVEL_TYPE_ATHLETE.name }
+      ],
+      targetLevelRules: [ (v) => !!v || this.$t('createRoom.targetLevel.required') ],
 
       introduction: this.$store.state.profile.introduction,
       introductionLavel: this.$t('profile.introduction.lavel'),
