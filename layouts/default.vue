e389660eb4370ad1ecaa952a70e129f6e7e4bf61
bba-99 <kenmochi@cardomain.co.jp>
2018-02-27 10:25:41 +0900

    update default

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,12 +34,6 @@
     </v-snackbar>
     <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
       <v-menu>
